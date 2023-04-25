bd72d97621329213ed4a9d40f401ddfa328b2d14
tome829 <gydnjs829@naver.com>
2023-04-25 19:09:49 +0900

    Initial commit

---
+++ src/main/java/inox/deokbae/deokbae/DeokbaeApplication.java
@@ -0,0 +1,13 @@
+package inox.deokbae.deokbae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeokbaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeokbaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inox/deokbae/deokbae/DeokbaeApplicationTests.java
@@ -0,0 +1,13 @@
+package inox.deokbae.deokbae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeokbaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
