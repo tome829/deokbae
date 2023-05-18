36fd31e38b76abe2b1d0e837a58b5117deaf72b1
mirae <mirae.konan@gmail.com>
2023-05-18 17:43:35 +0900

    프로필 이미지 추가

--- src/main/frontend/src/components/product/Product.js
+++ src/main/frontend/src/components/product/Product.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React from "react";//
 import imagePath from "../../img/chunsik3.jpg";
 
 function Product({item}) {
-    //const imagePath = `../../img/${item.image}`;
-    //const imagePath = `../../img/${item.image}`; //const imagePath = "src/img/" + ${item.image};
+    const imagePath = `../../img/${item.image}`;
+    //const imagePath = require(`../../img/${item.image}`).default;
+
+    console.log("image : " + imagePath);
 
     return (
         <div className="product">
