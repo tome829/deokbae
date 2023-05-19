ffda3a1f63e200c728b5b3a1e7b322cb1018dd82
mirae <mirae.konan@gmail.com>
2023-05-19 17:32:36 +0900

    dropdown 구현 중

--- src/main/frontend/src/components/header/Header.js
+++ src/main/frontend/src/components/header/Header.js
@@ -1,44 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import userImg from '../../img/icon/userImg.png';
 
-export default class Header extends React.Component{
-    state = { //로그인 여부를 상태로 관리 //기본값: false
-        isLoggedIn: false
-    }
+function Header() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    handleLogin = () => {
-        //로그인 처리 로직 구현 & 로그인 성공 시 isLoggedIn상태를 true로 업데이트
-        this.setState({isLoggedIn: true});
-    }
-    handleLogout = () => {
-        //로그인 처리 로직 구현 & 로그인 성공 시 isLoggedIn상태를 false로 업데이트
-        this.setState({isLoggedIn: false});
-    }
+    const handleLogin = () => {
+        // 로그인 처리 로직 구현 & 로그인 성공 시 isLoggedIn 상태를 true로 업데이트
+        setIsLoggedIn(true);
+    };
 
+    const handleLogout = () => {
+        // 로그아웃 처리 로직 구현 & 로그아웃 성공 시 isLoggedIn 상태를 false로 업데이트
+        setIsLoggedIn(false);
+    };
 
-    render() {
-        const {isLoggedIn} = this.state;
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
 
-        return(
-            <div className="header-main" id="header_main">
-                <div className="hd_main">
-                    <div className="hd_logo">
-                        로고
-                    </div>
-                    <div className="hd_per_page">
-                        {isLoggedIn ? (
-                            //로그인한 경우
-                            <>
-                                <img src={userImg} />
-                                <div onClick={this.handleLogout}>로그아웃</div>
-                            </>
-                        ) : (
-                            //로그인 하지 않은 경우
-                            <div onClick={this.handleLogin}>로그인</div>
-                        )}
-                    </div>
+    return (
+        <div className="header-main" id="header_main">
+            <div className="hd_main">
+                <div className="hd_logo">로고</div>
+                <div className="hd_per_page">
+                    {isLoggedIn ? (
+                        // 로그인한 경우
+                        <div onClick={toggleDropdown} className="dropdown-trigger">
+                            <img src={userImg} alt="User" />
+                            {isDropdownOpen && (
+                                <div className="dropdown">
+                                    <ul className="dropdown-list">
+                                        <li className="dropdown-item">설정</li>
+                                        <li className="dropdown-item" onClick={handleLogout}>
+                                            로그아웃
+                                        </li>
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
+                    ) : (
+                        // 로그인 하지 않은 경우
+                        <div onClick={handleLogin}>로그인</div>
+                    )}
                 </div>
             </div>
-        )
-    }
+        </div>
+    );
 }
+
+export default Header;
