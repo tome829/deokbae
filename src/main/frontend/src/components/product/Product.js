bbb0f7b8be1872fbe1a4f3745ba1530fcc6006a0
tome829 <gydnjs829@naver.com>
2023-05-19 13:50:47 +0900

    2023-05-19 FEATURE: 1. Product 사진 경로 수정

--- src/main/frontend/src/components/product/Product.js
+++ src/main/frontend/src/components/product/Product.js
@@ -1,13 +1,14 @@
 import React from "react";
-import imagePath from "../../img/chunsik3.jpg";
 
 function Product({item}) {
-    //const imagePath = `../../img/${item.image}`;
-    //const imagePath = `../../img/${item.image}`; //const imagePath = "src/img/" + ${item.image};
+    let path = `${item.image}`;
+    const imagePath = '/img/'+path;
 
+    console.log(item);
     return (
         <div className="product">
-            <img src={imagePath} alt="" />
+            <img src={imagePath} alt='이미지 테스트' />
+            <p>{imagePath}</p>
             <h2>{item.name}</h2>
             <p>{item.price}</p>
         </div>
