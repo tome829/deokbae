0bfcc8d628ee911d9fd9f42757980eaa81d78b64
mirae <mirae.konan@gmail.com>
2023-05-19 14:32:25 +0900

    header.js 수정

--- src/main/frontend/src/components/header/Header.js
+++ src/main/frontend/src/components/header/Header.js
@@ -2,7 +2,23 @@ import React from 'react';
 import userImg from '../../img/icon/userImg.png';
 
 export default class Header extends React.Component{
+    state = { //로그인 여부를 상태로 관리 //기본값: false
+        isLoggedIn: false
+    }
+
+    handleLogin = () => {
+        //로그인 처리 로직 구현 & 로그인 성공 시 isLoggedIn상태를 true로 업데이트
+        this.setState({isLoggedIn: true});
+    }
+    handleLogout = () => {
+        //로그인 처리 로직 구현 & 로그인 성공 시 isLoggedIn상태를 false로 업데이트
+        this.setState({isLoggedIn: false});
+    }
+
+
     render() {
+        const {isLoggedIn} = this.state;
+
         return(
             <div className="header-main" id="header_main">
                 <div className="hd_main">
@@ -10,7 +26,16 @@ export default class Header extends React.Component{
                         로고
                     </div>
                     <div className="hd_per_page">
-                        <img src={userImg} />
+                        {isLoggedIn ? (
+                            //로그인한 경우
+                            <>
+                                <img src={userImg} />
+                                <div onClick={this.handleLogout}>로그아웃</div>
+                            </>
+                        ) : (
+                            //로그인 하지 않은 경우
+                            <div onClick={this.handleLogin}>로그인</div>
+                        )}
                     </div>
                 </div>
             </div>
